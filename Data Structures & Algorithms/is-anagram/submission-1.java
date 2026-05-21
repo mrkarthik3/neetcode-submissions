12688e0a65a71d3004cbf346e494b60d69f8dab8
mrkarthik3 <48716076+mrkarthik3@users.noreply.github.com>
2026-05-21 17:34:01 +0530

    Add: is-anagram - submission-1

---
+++ Data Structures & Algorithms/is-anagram/submission-1.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        
+        // Time complexity = O(nlogn) due to sorting
+        // Space = O(n)
+        if(s.length() != t.length()) return false;
+
+        char[] sChars = s.toCharArray();
+        char[] tChars = t.toCharArray();
+        Arrays.sort(sChars);
+        Arrays.sort(tChars);
+
+        for(int i = 0; i < s.length(); i++) {
+            if(sChars[i] != tChars[i]) return false;
+        }
+
+        // It is anagram if all elements in both arrays are identical.
+        return true; 
+
+    }
+}
