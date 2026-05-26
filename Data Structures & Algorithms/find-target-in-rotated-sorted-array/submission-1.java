c28e235d526c0471a5f04c771e484d1be91387fc
mrkarthik3 <48716076+mrkarthik3@users.noreply.github.com>
2026-05-26 15:38:33 +0530

    Add: find-target-in-rotated-sorted-array - submission-1

---
+++ Data Structures & Algorithms/find-target-in-rotated-sorted-array/submission-1.java
@@ -0,0 +1,39 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        // O(log n) Time & O(1) Space
+        // Algo
+        // 1. find the pivot
+        // 2. perform Bin.Search on left and right parts
+        int l = 0;
+        int r = nums.length - 1;
+        while(l < r) {
+            int m = l + (r-l)/2;
+            if(nums[m] > nums[r]) {
+                l = m + 1;
+            } else {
+                r = m;
+            }
+        }
+        int pivot = l;
+        // l = pivot = min value in rotated sorted array.
+
+        int result = binarySearch(nums, target, 0, pivot-1);
+        if(result != -1) return result;
+
+        return binarySearch(nums, target, pivot, nums.length-1);
+    }
+
+    public static int binarySearch(int[] nums, int target, int l, int r) {
+        while(l<=r) {
+            int m = l+(r-l)/2;
+            if(nums[m] == target) {
+                return m;
+            } else if(nums[m] > target) {
+                r = m-1;
+            } else {
+                l = m+1;
+            }
+        }
+        return -1;
+    }
+}
